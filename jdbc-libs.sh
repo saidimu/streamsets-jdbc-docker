db319a0e9cd3e651d69e2679c925bd7258ee94ab
said apale <saidimu@gmail.com>
2017-01-14 18:39:11 -0500

    OpenSSL for wget over https

--- jdbc-libs.sh
+++ jdbc-libs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
+# install OpenSSL libs for wget https
+apk update
+apk --no-cache add openssl ca-certificates wget
+update-ca-certificates
+
 # check for mandatory environment variables and exit if not defined
 # [[ -z "$JDBC_LIB_URLS" ]] && { echo "Please specify comma-separated JDBC jar urls." ; exit 1; }
 [[ -z "$STREAMSETS_LIBRARIES_EXTRA_DIR" ]] && { echo "Please specify where to store the JDBC libraries." ; exit 1; }
 
 ## make sure we're in the dir that Streamsets expects the JDBC jars to be in
+## and that the dir exists or create it if it doesn't
+mkdir -p "$STREAMSETS_LIBRARIES_EXTRA_DIR/streamsets-datacollector-jdbc-lib/lib/"
 cd "$STREAMSETS_LIBRARIES_EXTRA_DIR/streamsets-datacollector-jdbc-lib/lib/"
 
 MYSQL_JDBC_URL="https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.0.8.tar.gz"
